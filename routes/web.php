ed80745e09d633d08f9919b34978e7d86ce58a7f
rizanoor <rizanoorfauzan123@gmail.com>
2024-04-27 06:09:06 +0700

    feat: slicing about page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AboutController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +9,5 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('about', [AboutController::class, 'index'])->name('about');
 
